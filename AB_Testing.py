ee223b40d797c9cdb0409ed1cacee0e9ca274f1f
Berkay İhsan Deniz <63191045+iambideniz@users.noreply.github.com>
2021-06-22 23:01:50 +0300

    Update AB_Testing.py

--- AB_Testing.py
+++ AB_Testing.py
@@ -2,11 +2,6 @@
 # AB Testing Project
 #################################################################
 
-# Business Problem:
-
-# Facebook recently introduced a new type of bid, average bidding, as an alternative to the current type of bidding called maximum bidding.
-# One of our customers, bombabomba.com, decided to test this new feature and wants to do an A/B test to see if average bidding converts more than maximum bidding.
-
 # Dataset Story:
 # In this dataset, which includes the website information of bombabomba.com, there is information such as the number of advertisements that users see and click and earnings information from here.
 # There are two separate data sets, Control and Test group. These datasets are in separate sheets of the ab_testing.xlsx excel.
