bfc078c26e69e9f7a5a0da9fb5e7fc119e00adc3
Berkay İhsan Deniz <63191045+iambideniz@users.noreply.github.com>
2021-06-22 23:02:43 +0300

    Update AB_Testing.py

--- AB_Testing.py
+++ AB_Testing.py
@@ -3,7 +3,7 @@
 #################################################################
 
 # Dataset Story:
-# In this dataset, which includes the website information of bombabomba.com, there is information such as the number of advertisements that users see and click and earnings information from here.
+# In this dataset, which includes the website information of XXXXXXXXXXX.com, there is information such as the number of advertisements that users see and click and earnings information from here.
 # There are two separate data sets, Control and Test group. These datasets are in separate sheets of the ab_testing.xlsx excel.
 
 # Variables:
